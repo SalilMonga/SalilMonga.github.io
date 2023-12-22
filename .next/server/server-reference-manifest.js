247b87b9bbd962d2c551be7e2379ee76664a158a
Salil Monga <monga.17@osu.edu>
2023-12-21 22:33:35 -0500

    Created a base website

---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"PNP2+G45F+emFsjtrWxl7vv+4mAclEDAOtORectuP9c=\"\n}"
---
+++ react-portfolio/src/secondPage/secondPage.tsx
@@ -0,0 +1,15 @@
+export const SecondPage = () => {
+  return (
+    <div>
+      <h3 className="text-4xl py-1">Services I offer</h3>
+      <p className="text-md py-2 leading-8 text-gray-80">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        <span className="text-purple-500"> veniam </span>, quis nostrud
+        exercitation ullamco laboris nisi ut{" "}
+        <span className="text-purple-500">aliquip ex ea commodo</span>
+        consequat.
+      </p>
+    </div>
+  );
+};
