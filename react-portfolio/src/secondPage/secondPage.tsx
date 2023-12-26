5a338f1ba8bd70b132d05b622751f5d65c0ea61e
Salil Monga <monga.17@osu.edu>
2023-12-25 19:54:29 -0500

    Added pdf and setup dark mode

--- react-portfolio/src/secondPage/secondPage.tsx
+++ react-portfolio/src/secondPage/secondPage.tsx
@@ -1,8 +1,8 @@
 export const SecondPage = () => {
   return (
     <div>
-      <h3 className="text-4xl py-1">Services I offer</h3>
-      <p className="text-md py-2 leading-8 text-gray-80">
+      <h6 className="text-8xl py-1 dark:text-white">Services I offer</h6>
+      <p className="text-md py-2 leading-8 text-gray-80 dark:text-white">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
         tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
         <span className="text-purple-500"> veniam </span>, quis nostrud
