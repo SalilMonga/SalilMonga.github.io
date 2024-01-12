e8e46a0890a85f30458856cb1dd165419a7e08c7
Salil Monga <monga.17@osu.edu>
2024-01-12 17:20:14 -0500

    feat: Added a button

--- react-portfolio/app/page.tsx
+++ react-portfolio/app/page.tsx
@@ -4,7 +4,7 @@ import { useEffect, useRef } from "react";
 import { Bounded } from "../src/component/Bounded";
 import { Shapes } from "./shapes/Shapes";
 import Link from "next/link";
-import { MdArrowOutward } from "react-icons/md";
+import { NavButton } from "./navButton/navButton";
 
 export default function Home() {
   const component = useRef(null);
@@ -61,9 +61,12 @@ export default function Home() {
   return (
     <Bounded ref={component}>
       <div className="ml-8 flex flex-col-reverse md:flex-row items-center content-center min-h-[70vh]">
-        <div className="w-full sm:w-1/2">
+        <div className="group w-full sm:w-1/2">
+          <div className="block md:hidden">
+            <NavButton />
+          </div>
           <h2
-            className="group mb-4 md:mb-8 text-[clamp(2rem,15vmin,20rem)] font-bold leading-none tracking-tighter"
+            className="mb-4 md:mb-8 text-[clamp(2rem,15vmin,20rem)] font-bold leading-none tracking-tighter"
             aria-label="Salil Monga"
           >
             <Link href="/aboutMe">
@@ -83,14 +86,6 @@ export default function Home() {
           <Shapes />
         </div>
       </div>
-      {/* TODO: Button for mobile */}
-      {/* <button className="group relative flex w-fit items-center justify-center overflow-hidden rounded-md border-2 border-slate-900 bg-slate-50  px-4 py-2 font-bold transition-transform ease-out  hover:scale-105">
-        <span className="absolute inset-0 z-0 h-full translate-y-9 bg-yellow-300 transition-transform  duration-300 ease-in-out group-hover:translate-y-0" />
-        <span className="relative flex items-center justify-center gap-2">
-          {" "}
-          {"Click Here" && <MdArrowOutward className="inline-block bg-black" />}
-        </span>
-      </button> */}
     </Bounded>
   );
 }
