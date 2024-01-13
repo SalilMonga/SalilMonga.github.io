9fad890e1373faf24b21c3005e8bda83982e8e66
Salil Monga <monga.17@osu.edu>
2024-01-13 17:20:09 -0500

    Bug: Routing is not fixed, deployment is fixed

--- react-portfolio/app/navButton/navButton.tsx
+++ react-portfolio/app/navButton/navButton.tsx
@@ -5,8 +5,9 @@ import Link from "next/link";
 
 export const NavButton = () => {
   const [isShown, setIsShown] = useState(false);
+
   return (
-    <Link href="/aboutMe">
+    <Link href="/aboutMe" passHref>
       <button
         className="group relative flex w-fit items-start justify-center overflow-hidden rounded-md border-2 border-slate-900 bg-slate-900 px-1 py-2 font-bold transition-transform ease-out 
       hover:scale-105"
