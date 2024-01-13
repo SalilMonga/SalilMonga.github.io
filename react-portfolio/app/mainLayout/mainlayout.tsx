f582ad1f5576c0ec960952ce3bde3e333baeff30
Salil Monga <monga.17@osu.edu>
2024-01-13 16:26:43 -0500

    feat: Warning user

--- react-portfolio/app/mainLayout/mainlayout.tsx
+++ react-portfolio/app/mainLayout/mainlayout.tsx
@@ -1,15 +1,37 @@
+import { useState } from "react";
 import Avatar from "./avatar/avatar";
 
 export const MainLayout = () => {
+  const [isShown, setIsShown] = useState(false);
   return (
     <div className="text-center p-4">
-      <a href="/">
+      <a
+        href="/"
+        onMouseEnter={() => {
+          setIsShown(true);
+        }}
+        onMouseLeave={() => {
+          setIsShown(false);
+        }}
+        onTouchStart={() => {
+          setIsShown(true);
+        }}
+        onTouchEnd={() => {
+          setIsShown(false);
+        }}
+      >
         <h2 className="hover:text-teal-400 text-5xl py-2 text-cyan-600 font-medium">
           Salil Monga
         </h2>
-        <p className="text-gray-500 font-mono font-extralight">
-          Clicking on the name will take you BACK!
-        </p>
+        {isShown ? (
+          <p className="text-gray-500 font-mono font-extralight">
+            Are you SUUREE...?
+          </p>
+        ) : (
+          <p className="text-gray-500 font-mono font-extralight">
+            Clicking on the name will take you BACK!
+          </p>
+        )}
       </a>
       <p className="text-md py-5 leading-8 text-gray-800 dark:text-white">
         My life revolves around finding a missing semi-colon in a thousand lines
