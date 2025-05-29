75cab5bf8e5693a21e021c7b26984b3b47449f40
Salil Monga <monga.17@osu.edu>
2025-05-29 13:45:44 -0700

    fix: deleted .next directory

--- .next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{\"/aboutMe\":[]},\"app\":{\"/Users/salilmonga/Personal Data/Projects/SalilMonga.github.io/app/layout\":[\"static/media/e4af272ccee01ff0-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
